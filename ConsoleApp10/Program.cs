2673baccb805dc1923d2dd06c693cdaf7ce32ea4
yusbaklee <67943334+yusbaklee@users.noreply.github.com>
2022-08-05 08:55:48 +0100

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class program
+{
+    public static void Main()
+    {
+        int[] arr = new int[] { 1, 9, 4, 8, 3, 7 };
+        arr = arr.OrderByDescending(a => a).ToArray();
+        foreach (int value in arr)
+        {
+            Console.WriteLine(value + " ");
+        }
+
+    }
+}
+
+        
